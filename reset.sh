759897106175bad09792d7e747121b3d88557041
Noah Stapp <noah@noahstapp.com>
2018-08-17 14:15:33 -0700

    Added reset.sh

---
+++ reset.sh
@@ -0,0 +1,9 @@
+git reset --hard HEAD
+touch instructions.txt
+echo edit instructions > instructions.txt
+echo edit README > README.md
+echo edit testOne > testOne.py
+echo edit testTwo > testTwo.py
+echo edit testThree > testThree.py
+echo edit testFour > testFour.py
+echo edit testFive > testFive.py
